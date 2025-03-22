1c6826f4c5c7cdeeed561f5e46d294fde3b84a03
Rutuja <rutujadautpure05@gmail.com>
2025-03-23 00:55:35 +0530

    handlers_array and pending_signal array added in stuct proc

--- proc.h
+++ proc.h
@@ -1,3 +1,4 @@
+#include "signal.h"
 // Per-CPU state
 struct cpu {
   uchar apicid;                // Local APIC ID
@@ -49,6 +50,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  sighandler_t handlers[NSIGS];
+  int pending_signals[NSIGS];
 };
 
 // Process memory is laid out contiguously, low addresses first:
---
+++ signal.h
@@ -0,0 +1,2 @@
+#define NSIGS 32
+
